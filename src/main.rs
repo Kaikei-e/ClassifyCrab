a1edfcf3e4cd2074e1c5b698b6edf543f8f6fbe6
Kaikei-e <e_kaikei@protonmail.com>
2021-12-16 22:32:21 +0900

    Update: tested reading the file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("testText.txt");
+    let display = path.display();
+
+    let mut f = match File::open(&path) {
+        Err(why) => panic!("couldn't open {} : {}", display, why),
+        Ok(f) => f,
+    };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {} : {}", display, why),
+        Ok(_) => println!("{} contains:\n{}", display, s)
+    }
+
 }
