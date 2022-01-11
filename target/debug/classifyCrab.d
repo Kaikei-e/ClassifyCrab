86090f46545d7497c0e3537c428187860705e0d3
Kaikei-e <e_kaikei@protonmail.com>
2022-01-11 21:51:26 +0900

    Update: add clap

---
+++ target/debug/classifyCrab.d
@@ -0,0 +1,1 @@
+/home/kokopeli/CLionProjects/classifyCrab/target/debug/classifyCrab: /home/kokopeli/CLionProjects/classifyCrab/src/main.rs
---
+++ target/debug/deps/classifyCrab-bd53004f9e1cd262.d
@@ -0,0 +1,5 @@
+/home/kokopeli/CLionProjects/classifyCrab/target/debug/deps/classifyCrab-bd53004f9e1cd262: src/main.rs
+
+/home/kokopeli/CLionProjects/classifyCrab/target/debug/deps/classifyCrab-bd53004f9e1cd262.d: src/main.rs
+
+src/main.rs:
