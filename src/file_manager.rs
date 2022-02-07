5d8b6c109a27be550d458ea874cdd639d607f891
Kaikei-e <e_kaikei@protonmail.com>
2022-02-08 01:09:40 +0900

    Update: Stacked. About borrow

--- src/file_manager.rs
+++ src/file_manager.rs
@@ -4,17 +4,16 @@ mod file_calculator;
 use file_searcher::zip_searcher;
 
 mod inspector{
-    zip_searcher!();
 
-    file_calculator!();
+    //file_calculator!();
 }
 
 mod extractor{
-    fn zip_extractor(){}
+    //fn zip_extractor(){}
 }
 
 mod classifier{
-    fn file_classifier(){}
+    //fn file_classifier(){}
 }
 
 
--- src/file_manager/file_searcher.rs
+++ src/file_manager/file_searcher.rs
@@ -2,19 +2,19 @@ use std::ffi::OsStr;
 use std::fs;
 use std::path::Path;
 
-pub fn zip_searcher(dir_path: &Path){
-    let mut file_list: Vec<&OsStr>  = vec![];
+pub fn zip_searcher(dir_path: &Path) {
+    let mut file_list: Vec<&OsStr> = vec![];
 
     for path in fs::read_dir(dir_path).unwrap() {
         let path = path.unwrap().path();
         let extension = path.extension().unwrap();
 
         if extension == "zip" {
-            file_list.push(*path.file_name())
+            let file_path = path.file_name().unwrap();
+
+            file_list.push(file_path.unwrap());
         }
     }
 
-    println!(file_list.len())
-
-
+    println!("{}", file_list.len())
 }
