3d113ec97c937bdb3e029367da00e8e0970cc0b9
Kaikei-e <e_kaikei@protonmail.com>
2022-02-08 23:42:01 +0900

    Update: Stacked. About borrow and Ownership.

--- src/file_manager/file_searcher.rs
+++ src/file_manager/file_searcher.rs
@@ -1,25 +1,20 @@
-use std::borrow::{Borrow, BorrowMut};
 use std::ffi::OsStr;
 use std::fs;
 use std::path::Path;
 
 pub fn zip_searcher(dir_path: &Path) {
-    //let fl_list_for_len = fs::read_dir(dir_path).iter();
-    //let file_length = fl_list_for_len.len();
-
     let mut file_list: Vec<&Path> = vec![];
 
     for path in match fs::read_dir(dir_path) {
         Ok(x) => x,
         Err(x) => panic!("{}", x),
     } {
+        let path_clone = path.clone().expect("Failed to clone file path.");
         let mut path_buf = path.unwrap().path();
         //let rpath = path;
 
         if is_zip_checker(path_buf.file_name().unwrap()) {
-            let path2 = path_buf.as_path();
-            let zip_path = zip_list_pusher(&mut path2.borrow());
-            file_list.push(zip_path)
+            file_list.push(path_clone.unwrap().path().borrow());
         }
     }
 
@@ -30,6 +25,6 @@ fn is_zip_checker(p: &OsStr) -> bool {
     return if p == OsStr::new("zip") { true } else { false };
 }
 
-fn zip_list_pusher<'a>(push_target: &'a mut &Path) -> &'a Path {
+fn zip_list_pusher(push_target: Box<Path>) -> Box<Path> {
     push_target
 }
