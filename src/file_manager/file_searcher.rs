fc977e52642b017819b4c6fcd54b9c8667ab8673
Kaikei-e <e_kaikei@protonmail.com>
2022-02-08 22:05:09 +0900

    Update: Stacked. About borrow

--- src/file_manager/file_searcher.rs
+++ src/file_manager/file_searcher.rs
@@ -1,20 +1,37 @@
 use std::ffi::OsStr;
+use std::fmt::Error;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 pub fn zip_searcher(dir_path: &Path) {
-    let mut file_list: Vec<&OsStr> = vec![];
+    //let fl_list_for_len = fs::read_dir(dir_path).iter();
+    //let file_length = fl_list_for_len.len();
 
-    for path in fs::read_dir(dir_path).unwrap() {
-        let path = path.unwrap().path();
-        let extension = path.extension().unwrap();
+    let mut file_list: Vec<&Path> = vec![];
 
-        if extension == "zip" {
-            let file_path = path.file_name().unwrap();
+    for path in match fs::read_dir(dir_path) {
+        Ok(x) => x,
+        Err(x) => panic!("{}", x),
+    } {
+        let path_buf = path.unwrap().path();
+        let path = path;
 
-            file_list.push(file_path.unwrap());
+        //let extension = path.extension().unwrap();
+        if is_zip_checker(path_buf.file_name().unwrap()){
+            let rpath = path.unwrap().path();
+            file_list.push(&*rpath);
+
         }
+
     }
 
     println!("{}", file_list.len())
+}
+
+fn is_zip_checker(p: &OsStr) -> bool {
+    return if p == OsStr::new("zip") {
+        true
+    }else {
+        false
+    }
 }
