f1f6be049423d6c395ec817d2f05ba935333ecd2
Kaikei-e <e_kaikei@protonmail.com>
2022-02-09 00:35:16 +0900

    Update: Stacked. About borrow and Ownership.

--- src/file_manager/file_searcher.rs
+++ src/file_manager/file_searcher.rs
@@ -1,6 +1,7 @@
+use std::borrow::{Borrow, BorrowMut};
 use std::ffi::OsStr;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 pub fn zip_searcher(dir_path: &Path) {
     let mut file_list: Vec<&Path> = vec![];
@@ -9,22 +10,28 @@ pub fn zip_searcher(dir_path: &Path) {
         Ok(x) => x,
         Err(x) => panic!("{}", x),
     } {
-        let path_clone = path.clone().expect("Failed to clone file path.");
-        let mut path_buf = path.unwrap().path();
-        //let rpath = path;
+        let path_buf: PathBuf = path.unwrap().path();
 
-        if is_zip_checker(path_buf.file_name().unwrap()) {
-            file_list.push(path_clone.unwrap().path().borrow());
+        let (return_path, boo) = is_zip_checker(Box::new(path_buf));
+
+        if boo {
+            file_list.push(&**zip_list_pusher(return_path));
         }
     }
 
     println!("{}", file_list.len());
 }
 
-fn is_zip_checker(p: &OsStr) -> bool {
-    return if p == OsStr::new("zip") { true } else { false };
+fn is_zip_checker(path: Box<PathBuf>) -> (PathBuf, bool) {
+    let pa = path.file_name().unwrap();
+
+    return if pa == OsStr::new("zip") {
+        (*path, true)
+    } else {
+        (*path, false)
+    };
 }
 
-fn zip_list_pusher(push_target: Box<Path>) -> Box<Path> {
+fn zip_list_pusher(push_target: PathBuf) -> PathBuf {
     push_target
 }
