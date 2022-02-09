6926acaaa0edc1eedaa96fadb6ef1263f001a95a
Kaikei-e <e_kaikei@protonmail.com>
2022-02-10 02:28:27 +0900

    Update: Made it!! Still need to learn how it works.

--- src/file_manager/file_searcher.rs
+++ src/file_manager/file_searcher.rs
@@ -4,18 +4,20 @@ use std::fs;
 use std::path::{Path, PathBuf};
 
 pub fn zip_searcher(dir_path: &Path) {
-    let mut file_list: Vec<&Path> = vec![];
+    let mut file_list: Vec<OsString> = vec![];
 
     for path in match fs::read_dir(dir_path) {
         Ok(x) => x,
         Err(x) => panic!("{}", x),
     } {
-        let mut pa = PathBuf::new();
-        let file_name = path.unwrap().file_name();
+        let mut pa = OsString::new();
+        let file_name = Box::new(path.unwrap().file_name());
         //let papa = &path.as_ref().unwrap();
         //let papapath = papa.to_owned();
         //let path_re = &papapath.path().clone();
-        file_list.push(is_zip_checker(file_name).as_ref());
+        file_list.push({pa = is_zip_checker(file_name);
+        pa
+        });
         //
         // if is_zip_checker(pa) {
         //     file_list.push(path_re.as_path());
@@ -26,14 +28,14 @@ pub fn zip_searcher(dir_path: &Path) {
 }
 
 //noinspection ALL
-fn is_zip_checker(p: OsString) -> OsString {
-    //let pa = p;
+fn is_zip_checker(p: Box<OsString>) -> OsString {
+    let pa = p.clone();
 
-    return if os_string_to_str(p).unwrap().to_string() == String::from("zip") {
-        let string1 = p.to_os_string();
+    return if os_string_to_str(*p) == String::from("zip") {
+        let string1 = pa.to_os_string();
         string1
     } else {
-        let string2 = p.to_os_string();
+        let string2 = pa.to_os_string();
         string2
     };
 
@@ -49,6 +51,6 @@ fn zip_list_pusher(push_target: &Path) -> &Path {
     push_target
 }
 
-fn os_string_to_str(input: OsString) -> Option<&'static str> {
-    input.to_str()
+fn os_string_to_str(input: OsString) -> String {
+    input.into_string().unwrap()
 }
